ce0c1db8f2d868d875f4125138884d3fb7862069
torres867 <shashankpandey867@gmail.com>
2022-10-17 06:54:08 +0530

    Test Case2: Hitting GET Request to fetch a particular record

---
+++ RestAssuredAPI_Automation/src/test/java/Org/Testing/TestCases1/TestCase2_GetRequest_ParticularRecord.java
@@ -0,0 +1,27 @@
+package Org.Testing.TestCases1;
+
+import static io.restassured.RestAssured.*;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+public class TestCase2_GetRequest_ParticularRecord 
+{
+	public static void main(String[] args) 
+	{
+		Response Res =
+		
+		given()
+		.contentType(ContentType.JSON)
+		.when()
+		.get("http://localhost:3000/PostmanAPIFolks/Shub123");
+		
+		System.out.println(" The Status Code of the GET Request is ");
+		System.out.println(Res.statusCode());
+		
+		System.out.println(" The Response is ");
+		System.out.println(Res.asString());
+		
+	}
+
+}
