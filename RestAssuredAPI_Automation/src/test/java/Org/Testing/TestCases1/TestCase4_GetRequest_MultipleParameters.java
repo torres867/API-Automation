686bd6a397174b6f54c25f2b2c8bc590849e9ef6
torres867 <shashankpandey867@gmail.com>
2022-10-17 07:13:59 +0530

    Test Case4:Get Request to fetch records using Multiple Parameters

---
+++ RestAssuredAPI_Automation/src/test/java/Org/Testing/TestCases1/TestCase4_GetRequest_MultipleParameters.java
@@ -0,0 +1,26 @@
+package Org.Testing.TestCases1;
+
+import static io.restassured.RestAssured.*;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+public class TestCase4_GetRequest_MultipleParameters 
+{
+	public static void main(String[] args) 
+	{
+		Response Res =
+		
+		given()
+		.contentType(ContentType.JSON)
+		.when()
+		.get("http://localhost:3000/PostmanAPIFolks?lastname=Toppo&id=Jaiwant123");
+		
+		System.out.println(" The Status Code of the GET Request is ");
+		System.out.println(Res.statusCode());
+		
+		System.out.println(" The Response is ");
+		System.out.println(Res.asString());
+	}
+
+}
